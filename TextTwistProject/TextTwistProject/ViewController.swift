cd9ce05ed48c8073f0eefbb12b12bce04cf5d870
Tiffany Obi <tiffanyobi123@gmail.com>
2019-11-13 17:41:02 -0500

    finished project just adding finishing touches. added win options and restart start option.

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -9,20 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var displayRandomWord: UILabel!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var inputTextField: UITextField!
     
     
     var getRandomWord = Word.getRandomWord().scrambled
     
     
+    //  var randomWordArray:[Character] = []
+    
+    // var emptyArr:[Character] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
+        inputTextField.delegate = self
+        nameLabel.text = "Try To Unscramble The Word!"
+        displayRandomWord.text = getRandomWord
+        
+        //  print(randomWordArray)
+        
+        
     }
-
-
+    
+    
 }
+
+extension ViewController: UITextFieldDelegate {
+    
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
+        
+        return true
+    }
+    
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if displayRandomWord.text?.contains(string) ?? false {
+            
+            
+            
+            displayRandomWord.text?.remove(at: (displayRandomWord.text?.firstIndex(of: Character(string)))!)
+            print(Word.getRandomWord().unscrambled)
+            
+        } else if string == "" {
+            displayRandomWord.text?.insert(textField.text?.last ?? "a", at: displayRandomWord.text!.endIndex)
+        } else {
+            
+            return false}
+        
+        
+        return true
+        
+        
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if inputTextField.text == Word.getRandomWord().unscrambled {
+            
+            nameLabel.text = "You Win!"
+        }
+        return true
+        
+    }
+    
+    
+    
+}
+
+
 
--- TextTwistProject/TextTwistProject/Word.swift
+++ TextTwistProject/TextTwistProject/Word.swift
@@ -19,3 +19,5 @@ struct Word {
         ].randomElement()!
     }
 }
+
+
